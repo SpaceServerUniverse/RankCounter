24787f31d5d7b4f79463231225f46f28000d6ccf
yurisi <Dev.yurisi@gmail.com>
2023-11-18 20:17:04 +0900

    first commit

---
+++ src/main/java/space/yurisi/rankcounter/RankCounter.java
@@ -0,0 +1,17 @@
+package space.yurisi.rankcounter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RankCounter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
